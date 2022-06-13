99c8af60a093dcb2365560f98ff1e31597a166e5
Антон Кулакевич <antonkulakevich@mail.ru>
2022-06-13 23:47:22 +0300

    Добавьте файлы проекта.

---
+++ HomeWork3. Exercise 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HomeWork3._Exercise_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ HomeWork3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.Intrinsics.Arm;
+
+namespace HomeWork3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число - ");
+            var num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0) Console.WriteLine("Число чётное");
+            else Console.WriteLine("Число нечётное") ;
+        }
+    }
+}
