f7c33b5d560fa552a4dcbfa46fb05cb76c8576e8
Антон Кулакевич <antonkulakevich@mail.ru>
2022-06-14 19:42:19 +0300

    End of home work. Waiting fot answer from my Mentor.

--- HomeWork3. Exercise 2/Program.cs
+++ HomeWork3. Exercise 2/Program.cs
@@ -1,12 +1,86 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace HomeWork3._Exercise_2
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            ///Use case
+            Console.WriteLine("Введите имя:"); string player = Console.ReadLine();
+
+            Console.WriteLine($"Здравствуйте - {player}, сколько у вас карт на руках?");
+            int numCards = int.Parse(Console.ReadLine());
+            int sum = 0;
+            
+            for (int i = 0; i < numCards; i++)
+            {
+                Console.WriteLine("Введите номинал карты - ");
+                string nom = Console.ReadLine();                //nom - номинал карт
+                switch (nom)
+                {
+                    case "2":
+                        sum += 2;
+                        break;
+
+                    case "3":
+                        sum += 3;
+                        break;
+                    
+                    case "4":
+                        sum += 4;
+                        break;
+                    
+                    case "5":
+                        sum += 5;
+                        break;
+
+                    case "6": 
+                        sum += 6;
+                        break;
+
+                    case "7":
+                        sum += 7;
+                        break;
+
+                    case "8":
+                        sum += 8;
+                        break;
+
+                    case "9":
+                        sum += 9;
+                        break;
+
+                    case "10":
+                        sum += 10;
+                        break;
+
+                    case "J":
+                        sum += 10;
+                        break;
+
+                    case "Q":
+                        sum += 10;
+                        break;
+
+                    case "K":
+                        sum += 10;
+                        break;
+
+                    case "T":
+                        sum += 10;
+                        break;
+                    default:
+                        Console.WriteLine("Out of Range");
+                        break;
+
+
+                }
+            }
+            Console.Clear();
+            if (sum == 21) Console.WriteLine("Сумма карт 21, вы выйграли!");
+            else Console.WriteLine($"Сумма карт - {sum}, вы не выйграли..");
+            Console.ReadKey();
         }
     }
 }
---
+++ HomeWork3. Exercise 3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HomeWork3._Exercise_3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите число - ");
+            int number = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            while (counter < number - 1)
+            {
+                counter++;
+                if (number % counter == 0)
+                {
+                    
+                    Console.WriteLine("Число с остатком!");
+                    break;
+                }
+                else if (number % counter !=0)
+                {
+                    Console.WriteLine("Число простое!");
+                    break;
+                }
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
