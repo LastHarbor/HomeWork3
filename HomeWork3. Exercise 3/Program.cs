31443a56627e183fa5c1f3e3109947396e573176
Антон Кулакевич <antonkulakevich@mail.ru>
2022-06-15 20:33:07 +0300

    Fixed exercise 3

--- HomeWork3. Exercise 3/Program.cs
+++ HomeWork3. Exercise 3/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Channels;
 
 namespace HomeWork3._Exercise_3
 {
@@ -9,24 +10,29 @@ namespace HomeWork3._Exercise_3
             Console.WriteLine("Введите число - ");
             int number = int.Parse(Console.ReadLine());
             int counter = 1;
+            bool flag = false;
+            
 
             while (counter < number - 1) //Счётчик чисел 
             {
                 counter++;
-                if (number % counter == 0)   //Проверка условий согласно тз
+                if (number % counter == 0 )   //Проверка условий согласно тз
                 {
-                    
-                    Console.WriteLine("Число с остатком!");
+                    flag = true;
                     break;
                 }
-                else if (number % counter !=0)
+                else if(counter%number == 1)
                 {
-                    Console.WriteLine("Число простое!");
+                    flag = false;
                     break;
                 }
                 
             }
 
+            if (flag == false) Console.WriteLine("Число простое");
+            else Console.WriteLine("Число с остатком");
+            ;
+            
             Console.ReadKey();
         }
     }
